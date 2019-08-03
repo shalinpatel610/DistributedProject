7b846006dabb82d71e376850321508a2967ea234
rahls7 <sharma.rahul71993@gmail.com>
2019-08-03 18:58:52 -0400

    Add instance 2

---
+++ src/server/instance2/logging/MyFormatter.java
@@ -0,0 +1,29 @@
+package server.instance3.logging;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class MyFormatter extends Formatter {
+
+	private static final DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+	 */
+	@Override
+	public String format(LogRecord record) {
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(df.format(new Date(record.getMillis()))).append(" - ");
+
+		builder.append("[").append(record.getLevel()).append("] - ");
+		builder.append(formatMessage(record));
+		builder.append("\n");
+		return builder.toString();
+	}
+
+}
---
+++ src/server/instance2/logging/MyLogger.java
@@ -0,0 +1,21 @@
+package server.instance3.logging;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyLogger {
+
+	static private FileHandler fileHandler;
+
+	public static void setup(final String logFile) throws IOException {
+		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+		logger.setUseParentHandlers(false);
+		logger.setLevel(Level.INFO);
+		fileHandler = new FileHandler(logFile, true);
+		fileHandler.setFormatter(new MyFormatter());
+		logger.addHandler(fileHandler);
+	}
+}
